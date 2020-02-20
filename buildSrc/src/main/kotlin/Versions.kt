cce4315b1dccaf1969c6d737217aec9ed77b4eca
Jean-Michel Fayard <jmfayard@gmail.com>
2020-02-20 10:50:28 +0100

    Replace manualy versions with _
    This time new versions are found
    
    However the build was changed because now the latest version is used instead to whatever was in the build before

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,60 +3,60 @@ object Kotlin {
 //    val version = "_"
 //        val version = "1.3.61"
 //    val version = "1.3.70-eap-42"
-    val version = "1.3.70-eap-184"
+    val version = "_"
 }
 
 object Data2Viz {
 //    const val version = "_"
-    const val version = "0.8.0-RC10"
+    const val version = "_"
     const val group = "io.data2viz.d2v"
 }
 
 object Ktor {
 //    const val version = "1.2.6"
-    const val version = "1.3.0"
+    const val version = "_"
 //    const val version = "1.3.0-rc3-1.3.70-eap-42"
 }
 
 object Logback {
-    const val version = "1.2.3"
+    const val version = "_"
 }
 
 object Serialization {
 //    const val version = "0.14.0"
 //    const val version = "0.14.0-1.3.70-eap-42"
-    const val version = "0.14.0-1.3.70-eap-134"
+    const val version = "_"
 }
 
 object Coroutines {
 //    const val version = "1.3.3"
-    const val version = "1.3.3-1.3.70-eap-42"
+    const val version = "_"
 }
 
 object KotlinLogging {
-    const val version = "1.7.8"
+    const val version = "_"
 }
 
 object KLogger {
-    const val version = "1.8.1"
+    const val version = "_"
 }
 
 object ReduxKotlin {
-    const val version = "0.2.9"
+    const val version = "_"
 }
 
 object KSvg {
-    const val version = "3.0.0-SNAPSHOT"
+    const val version = "_"
 }
 
 object Exposed {
-    const val version = "0.20.3"
+    const val version = "_"
 }
 
 object Postgres {
-    const val version = "42.2.2"
+    const val version = "_"
 }
 
 object Flyway {
-    const val version  = "6.2.1"
+    const val version  = "_"
 }
